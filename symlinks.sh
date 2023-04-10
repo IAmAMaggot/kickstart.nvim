ef737b566d253873866e8606ebf8a935489b7e94
IAmAMaggot <rasmus.mueller@gmail.com>
2023-04-10 13:07:19 +0200

    add symlink bash script

---
+++ symlinks.sh
@@ -0,0 +1,4 @@
+ln -s ~/.config/nvim/kickstart.nvim/init.lua ~/.config/nvim
+ln -s ~/.config/nvim/kickstart.nvim/lua ~/.config/nvim
+ln -s ~/.config/nvim/kickstart.nvim/pycodestyle ~/.config
+echo "created symlinks"
