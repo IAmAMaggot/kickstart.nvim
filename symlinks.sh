8ca32c8461c3db4831e5972368e90d55598884b6
IAmAMaggot <rasmus.mueller@gmail.com>
2023-08-14 17:48:00 +0200

    addtmuxconfig

--- symlinks.sh
+++ symlinks.sh
@@ -1,4 +1,6 @@
 ln -s ~/.config/nvim/kickstart.nvim/init.lua ~/.config/nvim
 ln -s ~/.config/nvim/kickstart.nvim/lua ~/.config/nvim
 ln -s ~/.config/nvim/kickstart.nvim/pycodestyle ~/.config
+ln -s ~/.config/nvim/kickstart.nvim/.tmux.conf ~/
+
 echo "created symlinks"
