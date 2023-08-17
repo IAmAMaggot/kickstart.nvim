71a1877666447b40f95ac5be3eb57addbad16e1e
IAmAMaggot <rasmus.mueller@gmail.com>
2023-08-17 17:57:08 +0200

    resize change

--- symlinks.sh
+++ symlinks.sh
@@ -1,6 +1,11 @@
-ln -s ~/.config/nvim/kickstart.nvim/init.lua ~/.config/nvim
-ln -s ~/.config/nvim/kickstart.nvim/lua ~/.config/nvim
-ln -s ~/.config/nvim/kickstart.nvim/pycodestyle ~/.config
-ln -s ~/.config/nvim/kickstart.nvim/.tmux.conf ~/
+# ln -s ~/.config/nvim/kickstart.nvim/init.lua ~/.config/nvim
+# ln -s ~/.config/nvim/kickstart.nvim/lua ~/.config/nvim
+# ln -s ~/.config/nvim/kickstart.nvim/pycodestyle ~/.config
+# ln -s ~/.config/nvim/kickstart.nvim/.tmux.conf ~/
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
+ln -s $SCRIPT_DIR/init.lua ~/.config/nvim
+ln -s $SCRIPT_DIR/lua ~/.config/nvim
+ln -s $SCRIPT_DIR/pycodestyle ~/.config
+ln -s $SCRIPT_DIR/.tmux.conf ~/
 
 echo "created symlinks"
