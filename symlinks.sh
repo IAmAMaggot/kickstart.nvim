341afef64cd2ab1b48188d2b4f9b6fa7d3a98320
IAmAMaggot <rasmus.mueller@gmail.com>
2023-08-18 09:15:20 +0200

    fix

--- symlinks.sh
+++ symlinks.sh
@@ -6,6 +6,6 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
 ln -s $SCRIPT_DIR/init.lua ~/.config/nvim
 ln -s $SCRIPT_DIR/lua ~/.config/nvim
 ln -s $SCRIPT_DIR/pycodestyle ~/.config
-ln -s $SCRIPT_DIR/.tmux.conf ~/
+ln -s $SCRIPT_DIR/dottmux.conf ~/.tmux.conf
 
 echo "created symlinks"
